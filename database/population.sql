60466a32196b7262da5f2527031dafa7a986c281
Douglas Nascimento <dnasc.i7sreact@proton.me>
2024-04-11 20:41:20 -0300

    Arquivo de população adicionado

---
+++ database/population.sql
@@ -0,0 +1,17 @@
+INSERT INTO Participante (nome_participante, cpf, id_endereco, id_grupo)
+VALUES ('Nome', 'cpf', 'Cardinal', 'Grupo');
+
+INSERT INTO Presente (tipo, nome_presente)
+VALUES ('Tipo', 'Nome');
+
+INSERT INTO Endereco (rua, numero_casa, cep)
+VALUES ('Rua', 'Numero', 'CEP');
+
+INSERT INTO Mensagem (conteudo)
+VALUES ('Conteudo');
+
+INSERT INTO Apelido (nome_apelido)
+VALUES ('Apelido');
+
+INSERT INTO AmigoSecreto (nome_grupo, data_sorteio, data_revelacao)
+VALUES ('NomeGrupo', '08092005', '08052004');
